ec6925ba24f66f4586e2610f8d25955ea837130e
Alex Mackenzie <alexbcmac7@gmail.com>
2017-09-21 16:25:04 -0700

    Remove unnecessary views

--- e-commerce-site/resources/views/cart/add.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: alexmac
- * Date: 21/09/17
- * Time: 4:09 PM
- */
--- e-commerce-site/resources/views/cart/show.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: alexmac
- * Date: 21/09/17
- * Time: 3:59 PM
- */
