3891a86ede2d3e563928076f336bcfb3419506d4
Alex Mackenzie <alexbcmac7@gmail.com>
2017-09-21 16:18:54 -0700

    Add fillables for cart, order and order item

--- e-commerce-site/app/Cart.php
+++ e-commerce-site/app/Cart.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    //
+    protected $fillable = [
+        'session_id',
+        'product_name',
+        'product_id',
+        'price',
+        'qty',
+    ];
 }
--- e-commerce-site/app/Order.php
+++ e-commerce-site/app/Order.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'order_identity',
+        'session_id',
+        'stripe_transaction_id',
+        'gross_total',
+        'status',
+    ];
 }
